82cb956e64fbf3e85de34abed7017714e8c8b7c1
comingaround <131857839+comingaround@users.noreply.github.com>
2025-11-10 16:40:26 +0200

    commit

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+import About01Hero from "@/components/about/about-01-hero/about-01-hero";
+
+export default function About() {
+    return(
+        <About01Hero />
+    );
+}
---
+++ components/about/about-01-hero/about-01-hero.tsx
@@ -0,0 +1,7 @@
+import "./about-01-hero.css";
+
+export default function About01Hero() {
+    return(
+        <h1 className="about-01hero-title">about</h1>
+    )
+}
